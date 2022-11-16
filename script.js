5d4af3afb72bad80fce0667751eed739b7d8ccbc
Tumelo Koko <tumelokoko10@gmail.com>
2022-11-16 21:10:08 +0200

    Feat: Show the iterator while choosing a show

--- script.js
+++ script.js
@@ -7,8 +7,29 @@ const randomShow = () => {
     const randomIndex = Math.floor(Math.random() * ourShows.length)
     if (parseInt(localStorage.getItem('previous')) != randomIndex) {
         const rShow = ourShows[randomIndex]
+        let start = -1
+        let tempShow
+        let interval = setInterval(() => {
+            tempShow = Math.floor(Math.random() * ourShows.length)
+            if (tempShow === start) {
+                if (tempShow - 1 > 0) {
+                    tempShow = tempShow - 1
+                } else {
+                    tempShow = tempShow + 1
+                }
+            }
+            show.textContent = ourShows[tempShow]
+            console.log(start, tempShow)
+            start = tempShow
+        }, 150)
+
+        setTimeout(() => {
+            clearInterval(interval)
+        }, 2000)
+
         show.textContent = rShow
         localStorage.setItem('previous', randomIndex)
+
     } else {
         randomShow()
     }
