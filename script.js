4b092ae0594789be63104f1ec12a2b73ec5e6a87
Tumelo Koko <tumelokoko10@gmail.com>
2022-11-16 21:24:42 +0200

    Upd: change the colours of the ui

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const ourShows = [`Bob's Burgers`, `Mob Psycho`, `Spy Family`, `Demon Slayers`, 
 
 let previousNumber
 const randomShow = () => {
+    show.style.color = '#1E1E1E'
     const randomIndex = Math.floor(Math.random() * ourShows.length)
     if (parseInt(localStorage.getItem('previous')) != randomIndex) {
         const rShow = ourShows[randomIndex]
@@ -25,6 +26,7 @@ const randomShow = () => {
 
         setTimeout(() => {
             clearInterval(interval)
+            show.style.color = '#D3202B'
         }, 2000)
 
         show.textContent = rShow
